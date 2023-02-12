1269ce9623a9c29315a2ac177ef045b8b565e76b
mirzoo <mirzo.okhunov@gmail.com>
2023-02-12 10:24:08 +0300

    add all pages

--- js/script.js
+++ js/script.js
@@ -5,8 +5,29 @@ $(document).ready(function() {
         $('body').toggleClass('lock')
     });
 
-
 });
 
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+var typed = new Typed (".auto-type", {
+  strings: ["innovative", "convenient", "the best"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true
+})
 
 
--- js/splider.js
+++ js/splider.js
@@ -1,20 +1,15 @@
-var splide = new Splide( '.splide', {
-    type   : 'loop',
-    perPage: 2,
-    focus  : 'center',
-    width: '393em',
-    fixedHeight: '38em',
-    gap: '24px',
-    autoplay: true,
-    interval: number = 2000,
-    pauseOnHover: true,
-    speed: number = 1000,
-    breakpoints: {
-      425: {
-        perPage: 1,
+const splide = new Splide('.splide', {
+    type: 'loop',
+    drag: 'free',
+    focus: 'center',
+    perPage: 7,
+    arrows: false,
+    pagination: false,
+    gap: 32,
+    autoScroll: {
+        speed: 1,
+    },
+});
 
-      },
-    }
-  } );
+splide.mount(window.splide.Extensions);
 
-  splide.mount();
